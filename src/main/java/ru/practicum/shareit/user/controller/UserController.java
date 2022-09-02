9b86401f11905d1ccc3fa8ebca8f241d290c264c
Александр <KraynovAB@yandex.ru>
2022-09-02 20:58:30 +0300

    Исправил замечания 1.2

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.user.controller;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
